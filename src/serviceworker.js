87900087f142f37ca535a4a39b4fa6bbc28dac87
max.4711 <github-commits@mail.studio-4711.com>
2020-12-01 12:59:02 +0100

    CHG: Service worker now able to work offline

--- src/serviceworker.js
+++ src/serviceworker.js
@@ -1,7 +1,44 @@
-self.addEventListener("install", event => {
-    console.log("Service Worker installing.");
-  });
+console.log("Hello from service worker");
+
+const cacheName = "offline-cache";
+
+const files = [
+    '.',
+    'about.html',
+    'app.js',
+    'helpers.js',
+    'icon.png',
+    'index.html',
+    'main.js',
+    'manifest.json',
+    'style.css'
+];
+
+self.addEventListener('install', (event) => {
+    event.waitUntil((async() => {
+        self.skipWaiting();
+        const cache = await caches.open(cacheName);
+        await cache.addAll(files);
+    })());
+});
   
-  self.addEventListener("activate", event => {
-    console.log("Service Worker activating.");
-  });
+self.addEventListener('activate', (event) => {
+    event.waitUntil((async() => {
+        self.clients.claim();
+    })());
+});
+
+self.addEventListener('fetch', (event) => {
+    event.respondWith((async () => {
+        const url = event.request.url;
+        const cache = await caches.open(cacheName);
+        const cachedResponse = await cache.match(url);
+        
+        if (cachedResponse) {
+            console.log(`From cache: ${url}`);
+            return cachedResponse;
+        }
+        console.log(`From network: ${url}.`);
+        return fetch(url);
+    })());
+});
